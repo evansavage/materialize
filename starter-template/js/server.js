4db8c81c5da7e5b294b8e59e6dcbe103ab1f734e
Evan <esav678@gmail.com>
2018-02-15 16:49:21 -0500

    Server edits v2

--- starter-template/js/server.js
+++ starter-template/js/server.js
@@ -3,7 +3,7 @@ const server = require('server');
 const { get, post } = server.router;
 
 // Launch server with options and a couple of routes
-server({ port: 1337 }, [
+server({ port: process.env.PORT || 1337 }, [
   get('/', ctx => 'Hello world'),
   post('/', ctx => console.log(ctx.data))
 ]);
